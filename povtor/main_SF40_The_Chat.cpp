f5033d3cbf0bff0a72fb281d3ab1914d98b7e9a6
Denis <fikinsd@gmail.com>
2024-04-13 16:01:44 +0300

    delete users

--- povtor/main_SF40_The_Chat.cpp
+++ povtor/main_SF40_The_Chat.cpp
@@ -1,16 +1,9 @@
 //#include "chat_class.h"
 #include <iostream>
 #include <vector>
-//#include "Users.h"
 #include "Chat111.h"
-#pragma warning(disable:4996)
 
 using namespace std;
-
-//string hill = "n";
-//string _login, _pass;
-//int set;
-//int _idUser = 1;
 
 //int main()
 //{
