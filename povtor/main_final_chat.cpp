889abdc12b56555d06f09ee60d8e9f93e40990b0
virtual_john@mail.ru <virtual_john@mail.ru>
2024-04-17 18:39:39 +0300

    nachalo. Menyate, kak hotite

--- povtor/main_final_chat.cpp
+++ povtor/main_final_chat.cpp
@@ -4,9 +4,43 @@
 
 using namespace std;
 
+void Registration()
+{
+    cout << "\n Let's do registration" << endl;
+    //....
+};
+
+void Main_Menu()
+{
+    short choice;
+    cout << "\t\t This is Chat!!" << endl;
+    cout << "1. Registration" << endl;
+    cout << "input choice";
+
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+    {
+        cout << "\n\t your choice: Registration "<< endl;
+        Registration();
+        break;
+    }
+
+    case 2:
+    {
+        //....
+        break;
+    }
+    }
+};
+
 int main()
 {
-	cout << "startup" << endl;
+	cout << "startup" <<endl << endl;
+
+    Main_Menu();
 
 	return 0;
 }
